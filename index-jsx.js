ce245db69653b25ef76252a7519c5e20f99e5025
Dacian Spinu <dacian.spinu@maxcode.net>
2016-06-03 12:04:15 +0300

    Component life cycles

--- index-jsx.js
+++ index-jsx.js
@@ -5,17 +5,78 @@ var FriendsContainer = React.createClass({
         friends: ["Vlad Tarcan", "Paul Bencko", "Ruxandra Bucos"]
       }
     },
+
+    addFriend: function(friend) {
+        this.state.friends.push(friend);
+        this.setState({
+          friends: this.state.friends
+        });
+    },
+
+    componentWillMount: function() {
+      alert("Component Will Mount")
+    },
+    componentDidMount: function() {
+      alert("Component Did Mount")
+    },
+    componentWillReceiveProps: function() {
+      alert("Component Will Receive Props")
+    },
+
+    componentWillUnmount: function() {
+      alert("Component Will Unmount")
+    },
     render: function() {
       return (
         <div>
           <h3>{ this.state.name }</h3>
-          <ShowList friends={ this.state.friends }/>
         </div>
       )
     }
 });
 
+var AddFriend = React.createClass({
+  getInitialState: function() {
+    return {
+      newFriend: ''
+    }
+  },
+
+  propTypes: {
+    addNew: React.PropTypes.func.isRequired
+  },
+
+  updateNewFriend: function(e) {
+    this.setState({
+      newFriend: e.target.value
+    })
+  },
+
+  handleAddNew: function() {
+    console.log(this.props)
+    this.props.addNew(this.state.newFriend);
+    this.setState({
+      newFriend: ''
+    })
+  },
+
+  render: function() {
+    return (
+      <div>
+        <input type="text" value={this.state.newFriend} onChange={this.updateNewFriend}></input>
+        <button onClick={this.handleAddNew}> Add Friend </button>
+      </div>
+    )
+  }
+});
+
+
 var ShowList = React.createClass({
+  getDefaultProps: function() {
+    return {
+      names: []
+    }
+  },
   render: function() {
     var listItems=this.props.friends.map(function(friend) {
       return <li> { friend } </li>
@@ -29,7 +90,7 @@ var ShowList = React.createClass({
       </div>
     )
   }
-})
+});
 
 ReactDOM.render(<FriendsContainer username="Tyler"/>, document.getElementById('app'));
 
