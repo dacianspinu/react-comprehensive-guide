9c0340625e47b1ff092450ce909da431d7dc533a
Dacian Spinu <dacian.spinu@maxcode.net>
2016-06-03 11:05:37 +0300

    jsx and compiled JS that utilizes child components & propagation of props

--- index-jsx.js
+++ index-jsx.js
@@ -1,14 +1,37 @@
-var HelloWorld = React.createClass({
-  render: function(){
+var FriendsContainer = React.createClass({
+    getInitialState: function() {
+      return {
+        name: "Dacian Spinu",
+        friends: ["Vlad Tarcan", "Paul Bencko", "Ruxandra Bucos"]
+      }
+    },
+    render: function() {
+      return (
+        <div>
+          <h3>{ this.state.name }</h3>
+          <ShowList friends={ this.state.friends }/>
+        </div>
+      )
+    }
+});
+
+var ShowList = React.createClass({
+  render: function() {
+    var listItems=this.props.friends.map(function(friend) {
+      return <li> { friend } </li>
+    });
     return (
       <div>
-        Hello World!
+        <h3>These are your friends</h3>
+        <ul>
+          {listItems}
+        </ul>
       </div>
     )
   }
-});
+})
 
-ReactDOM.render(<HelloWorld />, document.getElementById('app'));
+ReactDOM.render(<FriendsContainer username="Tyler"/>, document.getElementById('app'));
 
 //it seems like you can't run JSX code in browser directly so you actually need to transform that in JS code
 //by using babel --plugins transform-react-jsx index-jsx.js
--- index.js
+++ index.js
@@ -1,11 +1,50 @@
-var App = React.createClass({
+var FriendsContainer = React.createClass({
+  getInitialState: function () {
+    return {
+      name: "Dacian Spinu",
+      friends: ["Vlad Tarcan", "Paul Bencko", "Ruxandra Bucos"]
+    };
+  },
   render: function () {
     return React.createElement(
       "div",
       null,
-      "Hello World!"
+      React.createElement(
+        "h3",
+        null,
+        this.state.name
+      ),
+      React.createElement(ShowList, { friends: this.state.friends })
     );
   }
 });
 
-ReactDOM.render(React.createElement(App, null), document.getElementById('app'));
+var ShowList = React.createClass({
+  render: function () {
+    var listItems = this.props.friends.map(function (friend) {
+      return React.createElement(
+        "li",
+        null,
+        " ",
+        friend,
+        " "
+      );
+    });
+    return React.createElement(
+      "div",
+      null,
+      React.createElement(
+        "h3",
+        null,
+        "These are your friends"
+      ),
+      React.createElement(
+        "ul",
+        null,
+        listItems
+      )
+    );
+  }
+});
+
+ReactDOM.render(React.createElement(FriendsContainer, { username: "Tyler" }), document.getElementById('app'));
